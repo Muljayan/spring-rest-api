c97aee0e464372632808cceffc3da3c9640faee4
Muljayan <37667437+Muljayan@users.noreply.github.com>
2018-10-30 13:02:36 +0530

    Implemented validation

--- src/main/java/com/msj/webservices/springrestapi/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/msj/webservices/springrestapi/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.msj.webservices.springrestapi.exception;
 
 
 import com.msj.webservices.springrestapi.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import java.util.Date;
 @ControllerAdvice//Apply to all controllers
 @RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
@@ -31,5 +34,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                "Validation Failed",
+                ex.getBindingResult().toString());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
